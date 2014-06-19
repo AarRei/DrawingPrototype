19f757f427c24f084d266fe81aa89b8dc7557448
veN1337 <sascha.rothkopf@arcor.de>
2014-06-19 17:21:27 +0200

    stuff

--- src/xv/GUI/LayerConfigWindow.java
+++ src/xv/GUI/LayerConfigWindow.java
@@ -2,19 +2,22 @@ package xv.GUI;
 
 import java.awt.Dialog;
 
+import javax.swing.BoxLayout;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class LayerConfigWindow extends JDialog {
 	
-	private String[] connected_users;
+	private String[] connected_users = {"User1", "User2", "User3"};
+	JLabel lbl_txt = new JLabel("Select user who will be able to edit the layer:");
 
 	public LayerConfigWindow() {
-		int x = 200, y = 500;
+		int x = 200, y = 400;
 		
-		//TODO need method
-		//connected_users = getConnectedUsers();
-		
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+		this.setVisible(true);
 		
 	}
 
